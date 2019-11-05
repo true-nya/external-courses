3410977886bb3da8e83738ba9b5d7fd8c8324c8e
Anastasia Mironova <anast.mir22@yandex.ru>
2019-11-05 23:12:25 +0300

    feature: add task-02

---
+++ src/ex12_js_dom/task-02.js
@@ -0,0 +1,28 @@
+let userClick = document.querySelector(".user-menu");
+userClick.addEventListener('click', createSubMenu);
+function createSubMenu() {
+    console.log(!document.querySelector(".user-submenu"));
+    if (!document.querySelector(".user-submenu")) {
+        let arrow = document.querySelector(".arrow");
+        let ul = document.createElement("ul");
+        arrow.className += " reflected-arrow";
+        ul.className = "user-submenu";
+        userClick.append(ul);
+        for (i = 0; i < 3; i++) {
+            ul.append(document.createElement('li'))
+        }
+        let subItems = ul.children;
+        subItems[0].innerHTML = "<a href = '#'>My account</a>";
+        subItems[1].innerHTML = "<a href = '#'>My task</a>";
+        subItems[2].innerHTML = "<a href = '#'>Log out</a>";
+    } else {
+        closeMenu();
+    }
+
+}
+function closeMenu() {
+    let arrow = document.querySelector(".arrow");
+    arrow.className = "arrow";
+    let ul = document.querySelector(".user-submenu");
+    ul.parentNode.removeChild(ul);
+}
