178140c5e4e29b93df43dfd1b50d8639edcff3c4
Anastasia Mironova <anast.mir22@yandex.ru>
2019-11-09 22:31:58 +0300

    feature: add function for backlog and ready

--- src/ex13_js_events/task.js
+++ src/ex13_js_events/task.js
@@ -26,3 +26,94 @@ function closeMenu() {
     let ul = document.querySelector(".user-submenu");
     ul.parentNode.removeChild(ul);
 }
+
+localStorage.setItem("backlog", JSON.stringify({
+    title: "backlog",
+    issue: []
+}));
+localStorage.setItem("ready", JSON.stringify({
+    title: "ready",
+    issue: []
+}))
+// localStorage["inProgress"] = {
+//     title: "inProgress",
+//     issue: []
+// }
+// localStorage["finished"] = {
+//     title: "finished",
+//     issue: []
+// }
+
+document.querySelector(".backlog-button").addEventListener("click", addItemInBacklog);
+function addItemInBacklog() {
+    let currentTask = document.createElement("input");
+    let backlog = document.querySelector(".backlog-button");
+    currentTask.type = "text";
+    currentTask.className = "current-backlog-task";
+    backlog.before(currentTask);
+    currentTask.onchange = () => {
+        if (currentTask.value !== "") {
+            let currentObject = JSON.parse(localStorage.getItem("backlog"));
+            currentObject.issue.push({ id: "task" + currentObject.issue.length, name: currentTask.value });
+            localStorage.setItem("backlog", JSON.stringify(currentObject));
+            let task = document.createElement('div');
+            task.className = "task-item";
+            task.innerHTML = currentTask.value;
+            backlog.before(task);
+            currentTask.remove();
+        }
+    }
+};
+let readyButton = document.querySelector(".ready-button");
+readyButton.addEventListener("click", addItemInReady);
+function addItemInReady() {
+    if (JSON.parse(localStorage.getItem("backlog")).issue.length > 0) {
+        let currentTask = document.createElement("select");
+        let ready = document.querySelector(".body-ready");
+        currentTask.className = "current-ready-task";
+        ready.before(currentTask);
+
+        let currentObject = JSON.parse(localStorage.getItem("backlog"));
+        currentTask.append(new Option("choose task:"))
+        for (i = 0; i < currentObject.issue.length; i++) {
+            currentTask.append(new Option(currentObject.issue[i].name, currentObject.issue[i].id))
+        };
+
+
+
+        currentTask.onchange = () => {
+
+            let index;
+            let backlogObject = JSON.parse(localStorage.getItem("backlog"));
+            console.log(JSON.parse(localStorage.getItem("backlog")).issue);
+            index = currentTask.options.selectedIndex - 1;
+            console.log(index);
+            let readyObject = JSON.parse(localStorage.getItem("ready"));
+            let currentObj = Object.assign({}, backlogObject.issue[index]);
+            currentObj.id = 'task' + readyObject.issue.length;
+            backlogObject.issue.splice(index, 1);
+            localStorage.setItem("backlog", JSON.stringify(backlogObject));
+            readyObject.issue.push(currentObj);
+
+            let task = document.createElement('div');
+            task.className = "task-item";
+            task.innerHTML = currentObj.name;
+            ready.before(task);
+            currentTask.remove();
+        };
+    };
+};
+
+window.addEventListener("load", initialization)
+function initialization() {
+    let backlog = document.querySelector(".body-backlog");
+    if (JSON.parse(localStorage.getItem("backlog")).issue.length > 0) {
+        let currentObject = JSON.parse(localStorage.getItem("backlog"));
+        for (i = 0; i < currentObject.issue.length; i++) {
+            let task = document.createElement('div');
+            task.className = "task-item";
+            task.innerHTML = currentObject.issue[i];
+            backlog.before(task);
+        }
+    }
+}
